03be6d4a601a6ac35e040e4a461daf1d9093f349
baekCode <sosaer88@gmail.com>
2020-03-12 15:19:00 +0900

    add, 서버데이터 전달, itemModel 구현

--- src/models/itemModel.js
+++ src/models/itemModel.js
@@ -8,9 +8,11 @@ export default class ItemModel extends Observable {
     this.menu = null;
     // this.selectedItem = null;
   }
-
   getInitialData() {
-    this.http.get(this.url);
+    this.http.get(this.url).then(data => {
+      this.menu = data;
+      this.notify('onLoad', this.menu);
+    });
     // response받은 데이터를 this.menu에 할당
     // 데이터 로드가 완료되면 notify 메소드 실행하여 observers(Views) 업데이트
   }
