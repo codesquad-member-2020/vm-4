5f723c2fae2475df52db2541e7ff3eb217e3cab6
Reese Kim <reesekimm@gmail.com>
2020-03-20 12:05:16 +0900

    modify, PATCH 라우터 삭제

--- src/server/router.js
+++ src/server/router.js
@@ -1,24 +1,12 @@
 import express from "express";
 import routes from "./routes.js";
-import fs from "fs";
-import path from "path";
 
 const menuJSON = require("../db/menu.json");
 const walletJSON = require("../db/wallet.json");
 
 const router = express.Router();
 
-const updateWalletJson = (req, res) => {
-  const latestData = req.body;
-  const file = path.join(__dirname, "../db/wallet.json");
-  fs.writeFile(file, JSON.stringify(latestData, null, 2), err => {
-    if (err) throw err;
-    res.send(walletJSON);
-  });
-};
-
 router.get(routes.items, (req, res) => res.send(menuJSON));
 router.get(routes.wallet, (req, res) => res.send(walletJSON));
-router.patch(routes.wallet, updateWalletJson);
 
 export default router;
