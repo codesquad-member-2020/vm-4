3a6f8a21812950096a45f9fc884227bc7b09ccb9
Reese Kim <reesekimm@gmail.com>
2020-03-20 15:43:23 +0900

    feat, localStorage에 wallet data 캐싱

--- src/models/walletModel.js
+++ src/models/walletModel.js
@@ -11,14 +11,26 @@ export default class WalletModel extends Observable {
   }
 
   getInitialData() {
+    const walletDB = JSON.parse(localStorage.getItem("walletDB"));
+    if (walletDB) {
+      this.moneyList = walletDB.moneyList;
+      this.total = walletDB.total;
+      this.notify(observerType.loadData, { moneyList: this.moneyList, total: this.total });
+      return;
+    }
+    this.fetchData();
+  }
+
+  fetchData() {
     const { url, http } = this;
     http.get(url).then(data => {
-      this.setData(data);
+      this.setDefaultData(data);
+      this.updateLocalStorage(data, this.total);
       this.notify(observerType.loadData, { moneyList: this.moneyList, total: this.total });
     });
   }
 
-  setData(data) {
+  setDefaultData(data) {
     let total = 0;
     for (const money in data) {
       total += money * data[money];
@@ -27,11 +39,27 @@ export default class WalletModel extends Observable {
     this.moneyList = data;
   }
 
+  updateLocalStorage(moneyList, total) {
+    const isObject =
+      typeof moneyList === "object" && !Array.isArray(moneyList) && moneyList !== null;
+    const isNumber = typeof total === "number" && total >= 0;
+    try {
+      if (isObject && isNumber) {
+        localStorage.setItem("walletDB", JSON.stringify({ moneyList, total }));
+      } else {
+        throw Error("유효한 데이터가 아닙니다.");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   updateWhenInputMoney(money) {
     const { moneyList } = this;
     if (moneyList[money] === 0) return;
     moneyList[money] -= 1;
     this.total -= money;
+    this.updateLocalStorage(moneyList, this.total);
     this.notify(observerType.inputMoney, { moneyList: moneyList, total: this.total });
   }
 
@@ -45,12 +73,7 @@ export default class WalletModel extends Observable {
       }
     }
     moneyTypeList.forEach(processChanges.bind(this));
-    // this.requestUpdate(this.moneyList);
+    this.updateLocalStorage(this.moneyList, this.total);
     this.notify(observerType.purchaseItem, { moneyList: this.moneyList, total: this.total });
-  }
-
-  requestUpdate(data) {
-    const { url, http } = this;
-    http.patch(url, data);
   }
 }
