16c14146ea4cc0611ff3baf146355312a94c1ccb
Reese Kim <reesekimm@gmail.com>
2020-03-21 16:28:52 +0900

    deploy

--- src/init.js
+++
@@ -1,4 +0,0 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import "./style/style.scss";
-import "./js/main.js";
--- src/util/utilFunc.js
+++
@@ -1,4 +0,0 @@
-export const $select = el => document.querySelector(el);
-export const $selectAll = el => document.querySelectorAll(el);
-export const $getById = id => document.getElementById(id);
-export const $addListener = (target, type, callback) => target.addEventListener(type, callback);
